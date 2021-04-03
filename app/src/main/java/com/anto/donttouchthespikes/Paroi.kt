be6305f680aa642736d81a8ccc4607e1711985e4
antoine4325 <antoine4325@gmail.com>
2021-04-03 23:55:47 +0200

    Rédaction Paroi.kt + initialisations des parois dans le DrawingView.kt

--- app/src/main/java/com/anto/donttouchthespikes/Paroi.kt
+++ app/src/main/java/com/anto/donttouchthespikes/Paroi.kt
@@ -1,5 +1,16 @@
 package com.anto.donttouchthespikes
 
-class Paroi (view: DrawingView) {
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
 
+class Paroi (x1: Float, y1: Float, x2: Float, y2: Float) {
+    val paroi = RectF(x1, y1, x2, y2)
+    val paroiPaint = Paint()
+
+    fun draw(canvas: Canvas) {
+        paroiPaint.color = Color.GRAY
+        canvas.drawRect(paroi, paroiPaint)
+    }
 }
