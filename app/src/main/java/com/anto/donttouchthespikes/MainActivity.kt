e4e885ea97cfa99dec3cc60e6f9eb57126b64434
antoine4325 <antoine4325@gmail.com>
2021-04-03 22:52:50 +0200

    Ajout Paroi.kt + toutes les méthodes utiles dans DrawingView.kt + la base dans MainActivity.kt

--- app/src/main/java/com/anto/donttouchthespikes/MainActivity.kt
+++ app/src/main/java/com/anto/donttouchthespikes/MainActivity.kt
@@ -4,8 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    lateinit var view: DrawingView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        view = findViewById(R.id.surfaceView)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        view.pause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        view.resume()
     }
 }
---
+++ app/src/main/java/com/anto/donttouchthespikes/Paroi.kt
@@ -0,0 +1,5 @@
+package com.anto.donttouchthespikes
+
+class Paroi (view: DrawingView) {
+
+}
