9e51a74f3acd31e7f36cde4a336abfd3f77428dd
meirama <merys05@outlook.fr>
2021-05-05 18:23:03 +0200

    v05/05

--- app/src/main/java/com/anto/donttouchthespikes/Spikes.kt
+++ app/src/main/java/com/anto/donttouchthespikes/Spikes.kt
@@ -12,6 +12,7 @@ class Spikes(val view: DrawingView){
     var re = RectF(0f, 0f, 0f, 0f)
     val re0 = RectF(0f, 0f, 0f, 0f)
     var liste1: MutableList<RectF> = mutableListOf(re0)
+    var liste2: MutableList<RectF> = mutableListOf(re0)
 
      fun drawSpikeParoi() {
          var x = 105F
@@ -44,27 +45,21 @@ class Spikes(val view: DrawingView){
              x += width
          }
      }
-     fun interspikes(oiseau: Oiseau) {
-        for (i in liste1) {
-            if (oiseau.r.contains(i)) view.gameOver()
-        }
 
-     }
      fun drawSpikesLeft() {
          var nbresplibre = 6 - view.oiseau.niveau
          //var nbrSpikes = 13 - 2*nbresplibre
-         var nbrSpikes = 6
+         var nbrSpikes = 3
          val x = 110F
          var y = 280F
          val width = 125F
          val halfWidth = width / 2
-         var k = (2..7).random()
+         var k = (1..12).random()
 
          if (view.oiseau.vx <= 0) {
-             val elem = liste1.size
-             liste1.dropLast(elem-1)
+             view.spikes.liste1 = mutableListOf(re0)
              for (i in 1..nbrSpikes) {
-                 //y = 280F + (3 * width)
+
                  var x1 = x - halfWidth
                  var y1 = y + halfWidth
                  var x2 = x - halfWidth
@@ -78,9 +73,8 @@ class Spikes(val view: DrawingView){
                  re = RectF(x1 + 10, y1, x + 10 + (width/50), y2)
                  liste1.add(re)
                  paint.color = Color.DKGRAY
-                 //y = 280F + (k * width)
-                 y += 2*width
-                 k = (2..7).random()
+                 y = 280F + (k * width)
+                 k = (1..12).random()
              }
 
                  //view.canvas.save()
@@ -93,21 +87,18 @@ class Spikes(val view: DrawingView){
              path.lineTo(120F - (125F/2), 280F + (3 * 125F) - (125F/2))
              path.moveTo(120F - (125F/2) , 280F + (3 * 125F) + (125F/2))
              path.close()*/
-
-
          }
-
-
-
      }
-     fun drawSpikesRight(nbrSpikes: Int, k: Int){
+     fun drawSpikesRight(){
          val x = view.screenWidth - 50F
          var y = 280F
          val width = 125F
          val halfWidth = width / 2
+         var nbrSpikes = 6
+         var k = (1..12).random()
          if (view.oiseau.vx >= 0) {
-             //val elem = liste2.size
-             //liste2.dropLast(elem-1)
+             val elem = liste2.size
+             view.spikes.liste2 = mutableListOf(re0)
              for (i in 1..nbrSpikes) {
                  path.moveTo(x - halfWidth, y)
                  path.lineTo(x ,y - halfWidth)
@@ -116,11 +107,12 @@ class Spikes(val view: DrawingView){
                  path.close()
                  //view.canvas.save()
                  //view.canvas.rotate(45F)
-                 //re = RectF(x - halfWidth, y + halfWidth, x + halfWidth, y - halfWidth)
+                 re = RectF(x - halfWidth -10, y + halfWidth, x + 10 + (width/50), y - halfWidth)
                  //view.canvas.restore()
-                 //liste1.add(re)
+                 liste2.add(re)
                  paint.color = Color.DKGRAY
                  y = 280F + (k * width)
+                 k = (1..12).random()
              }
          }
 
