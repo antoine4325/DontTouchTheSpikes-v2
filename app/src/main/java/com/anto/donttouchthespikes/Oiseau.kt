bb445a9ab04ee4c5feac5cc8056f764f1dfde294
antoine4325 <antoine4325@gmail.com>
2021-05-06 18:02:35 +0200

    Petite rectif sur la position des bonbons

--- app/src/main/java/com/anto/donttouchthespikes/Oiseau.kt
+++ app/src/main/java/com/anto/donttouchthespikes/Oiseau.kt
@@ -61,10 +61,10 @@ class Oiseau(val echelle : Float, val view: DrawingView, context: Context): View
         view.checkColor()
         if (view.nbrTouche == view.bonbon.nextVisible) {
             if (vx < 0) {
-                view.bonbon.carre.offsetTo(150f, random.nextFloat()*view.screenHeight*0.8f+150)
+                view.bonbon.carre.offsetTo(150f, random.nextFloat()*view.screenHeight*0.7f+150)
             }
             else {
-                view.bonbon.carre.offsetTo(view.screenWidth-270f, random.nextFloat()*view.screenHeight*0.8f+150)
+                view.bonbon.carre.offsetTo(view.screenWidth-270f, random.nextFloat()*view.screenHeight*0.7f+150)
             }
             view.bonbon.visible = true
         }
