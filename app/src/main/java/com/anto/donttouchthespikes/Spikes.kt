9106d3659e5ab9aef5060ee5eb8a476cd6686df1
antoine4325 <antoine4325@gmail.com>
2021-05-06 23:51:05 +0200

    C'est maintenant l'intersection avec un rectangle invisible qui déclenche la perte d'une vie, et pas les parois du haut et du bas en elles-mêmes. Création fonction reset dans Spikes.kt pour reset le rectangle invisible, fonction ouch dans oiseau quand on touche une pique

--- app/src/main/java/com/anto/donttouchthespikes/Spikes.kt
+++ app/src/main/java/com/anto/donttouchthespikes/Spikes.kt
@@ -6,13 +6,13 @@ import android.graphics.drawable.shapes.RectShape
 import androidx.constraintlayout.solver.widgets.Rectangle
 
 
-class Spikes(val view: DrawingView){
+class Spikes(val view: DrawingView) {
     val paint = Paint()
     var path = Path()
     var re = RectF(0f, 0f, 0f, 0f)
     val re0 = RectF(0f, 0f, 0f, 0f)
-    val rhaut = RectF(0f,0f, view.screenWidth, 200f) //Pics haut
-    val rbas = RectF(0f, view.screenHeight - 205f, view.screenWidth, view.screenHeight) //Pics bas
+    var rhaut = RectF(0f,0f, 0f, 0f) //Pics haut
+    var rbas = RectF(0f, 0f, 0f, 0f) //Pics bas
     var liste1: MutableList<RectF> = mutableListOf(re0)
     var liste2: MutableList<RectF> = mutableListOf(re0)
     val nbresplibre = 6 - view.oiseau.niveau
@@ -21,73 +21,78 @@ class Spikes(val view: DrawingView){
     val width = 125F
     val halfWidth = width / 2
 
-     fun drawSpikeParoi() {
-         var x = 105F
-         var y = 155F
+    fun drawSpikeParoi() {
+        var x = 105F
+        var y = 155F
 
-         for (i in 1..8) {
-             path.moveTo(x, y + halfWidth)
-             path.lineTo(x - halfWidth, y)
-             path.lineTo(x + halfWidth, y)
-             path.moveTo(x, y + halfWidth)
-             path.close()
-             paint.color = Color.DKGRAY
-             x += width
-         }
-         x = 105F
-         y = view.screenHeight - 155
-         for (i in 1..8) {
-             path.moveTo(x, y - halfWidth)
-             path.lineTo(x - halfWidth, y)
-             path.lineTo(x + halfWidth, y)
-             path.moveTo(x, y - halfWidth)
-             path.close()
-             paint.color = Color.DKGRAY
-             x += width
-         }
-     }
+        for (i in 1..8) {
+            path.moveTo(x, y + halfWidth)
+            path.lineTo(x - halfWidth, y)
+            path.lineTo(x + halfWidth, y)
+            path.moveTo(x, y + halfWidth)
+            path.close()
+            paint.color = Color.DKGRAY
+            x += width
+        }
+        x = 105F
+        y = view.screenHeight - 155
+        for (i in 1..8) {
+            path.moveTo(x, y - halfWidth)
+            path.lineTo(x - halfWidth, y)
+            path.lineTo(x + halfWidth, y)
+            path.moveTo(x, y - halfWidth)
+            path.close()
+            paint.color = Color.DKGRAY
+            x += width
+        }
+    }
 
-     fun drawSpikesLeft() {
-         val x = 110F
-         var y = 280F
+    fun drawSpikesLeft() {
+        val x = 110F
+        var y = 280F
 
-         if (view.oiseau.vx <= 0) {
-             view.spikes.liste1 = mutableListOf(re0)
-             for (i in 1..nbrSpikes) {
-                 path.moveTo(x-halfWidth, y+halfWidth)
-                 path.lineTo(x-halfWidth, y-halfWidth)
-                 path.lineTo(x , y)
-                 path.moveTo(x-halfWidth, y+halfWidth)
-                 path.close()
-                 re = RectF(x-halfWidth + 40 , y+halfWidth - 40, x-halfWidth + 60, y-halfWidth + 40)
-                 liste1.add(re)
-                 paint.color = Color.DKGRAY
-                 y = 280F + (k * width)
-                 k = (1..12).random()
-             }
-         }
-     }
-     fun drawSpikesRight(){
-         val x = view.screenWidth - 50F
-         var y = 280F
+        if (view.oiseau.vx <= 0) {
+            view.spikes.liste1 = mutableListOf(re0)
+            for (i in 1..nbrSpikes) {
+                path.moveTo(x-halfWidth, y+halfWidth)
+                path.lineTo(x-halfWidth, y-halfWidth)
+                path.lineTo(x , y)
+                path.moveTo(x-halfWidth, y+halfWidth)
+                path.close()
+                re = RectF(x-halfWidth + 40 , y+halfWidth - 40, x-halfWidth + 60, y-halfWidth + 40)
+                liste1.add(re)
+                paint.color = Color.DKGRAY
+                y = 280F + (k * width)
+                k = (1..12).random()
+            }
+        }
+    }
 
-         if (view.oiseau.vx >= 0) {
-             view.spikes.liste2 = mutableListOf(re0)
-             for (i in 1..nbrSpikes) {
-                 path.moveTo(x - halfWidth, y)
-                 path.lineTo(x ,y - halfWidth)
-                 path.lineTo(x ,y + halfWidth)
-                 path.moveTo(x - halfWidth, y)
-                 path.close()
-                 re = RectF(x - halfWidth , y  + halfWidth - 40, x - halfWidth + 20, y - halfWidth + 40)
-                 liste2.add(re)
-                 paint.color = Color.DKGRAY
-                 y = 280F + (k * width)
-                 k = (1..12).random()
-             }
-         }
+    fun drawSpikesRight(){
+        val x = view.screenWidth - 50F
+        var y = 280F
 
-     }
+        if (view.oiseau.vx >= 0) {
+            view.spikes.liste2 = mutableListOf(re0)
+            for (i in 1..nbrSpikes) {
+                path.moveTo(x - halfWidth, y)
+                path.lineTo(x ,y - halfWidth)
+                path.lineTo(x ,y + halfWidth)
+                path.moveTo(x - halfWidth, y)
+                path.close()
+                re = RectF(x - halfWidth , y  + halfWidth - 40, x - halfWidth + 20, y - halfWidth + 40)
+                liste2.add(re)
+                paint.color = Color.DKGRAY
+                y = 280F + (k * width)
+                k = (1..12).random()
+            }
+        }
 
+    }
+
+    fun reset(sW: Float, sH: Float) {
+        rhaut = RectF(0f,0f, sW, 200f) //Pics haut
+        rbas = RectF(0f, sH - 205f, sW, sH) //Pics bas
+    }
 
 }
